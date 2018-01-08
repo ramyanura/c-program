43afb18b6398e920a0ff30d3803032d78848b5a8
ramyanura <31505721+ramyanura@users.noreply.github.com>
2018-01-08 20:36:24 +0530

    Create vowels.c

---
+++ vowels.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main()
+{
+	char ch;
+	scanf("%s",&ch);
+	if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+	{
+		printf("vowels");
+	
+	}
+	else
+	{
+		printf("consonants");
+	}
+}
