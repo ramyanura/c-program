ca4a22cbef27b46d7e92909aeea3a782d8aaad3f
ramyanura <31505721+ramyanura@users.noreply.github.com>
2018-01-24 16:37:19 +0530

    Create arth.c

---
+++ arth.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int n,a,d,sum=0,i,y=o;
+	scanf("%d",&n);
+	scanf("%d",&a);
+	scanf("%d",&d);
+	for(i=a;i<=n;i=i+d)
+	{
+		sum=sum+i;
+	}
+	print("%d",sum);
+	
+}return 0;
