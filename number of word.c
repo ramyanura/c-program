dcc148f466eb41909211f4a2010b70f82f7aef57
ramyanura <31505721+ramyanura@users.noreply.github.com>
2018-02-09 15:38:21 +0530

    Update number of word.c

--- number of word.c
+++ number of word.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 int main()
 {
-	int i,count=1;
-	char a;
+	int i,count=0;
+	char a[100];
 	scanf("%[^\n]s",&a);
-	for(i=0;i<=a;i++)
+	for(i=0;a[i]!='\0';i++)
 {
 	if(a[i]==' ')
 	{
       count++;
 	}
 }
-	printf("%d",count);
+	printf("%d",count+1);
 	return 0;
 }
