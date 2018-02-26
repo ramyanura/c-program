898957779b7822f9e70d10470150501a5349d918
ramyanura <31505721+ramyanura@users.noreply.github.com>
2018-02-26 11:13:41 +0000

    Create nearest even number.c

---
+++ nearest even number.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n,t;
+    scanf("%d",&n);
+    t=n-1;
+    if(t%2==0)
+    {
+        printf("%d",t);
+    
+    }
+    else
+    {
+        printf("%d",n);
+    }
+    return 0;
+}
