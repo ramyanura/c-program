119f7799465d8379198fc763f51e96cfc517882b
ramyanura <31505721+ramyanura@users.noreply.github.com>
2018-03-19 10:27:58 +0000

    Create 72.c

---
+++ 72.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	char s;
+	scanf("%s",&s);
+	if(s=='a'||s=='e'||s=='i'||s=='o'||s=='u')
+	{
+		printf("yes");
+	}
+	else
+	{
+		printf("no");
+	}
+	return 0;
+}
